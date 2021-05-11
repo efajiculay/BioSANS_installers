8eb3a5abb762bc2bbb2d8d9ea9142c70aaed73c4
efajiculay <74398236+efajiculay@users.noreply.github.com>
2021-05-12 02:18:39 +0800

    Delete BioSANS_installer.bat

--- Windows/common/BioSANS_installer.bat
+++
@@ -1,8 +0,0 @@
-:: https://silentinstallhq.com/python-3-9-silent-install-how-to-guide/
-:: https://www.tutorialspoint.com/batch_script/
-:: start python-3.9.x.exe /uninstall /quiet
-
-:: @echo off
-mkdir C:\BioSANS2021
-start /WAIT .\python-3.9.5-amd64.exe /quiet .\unattend.xml
-start /B /WAIT C:\BioSANS2021\Python39\python.exe Window_Installer.py
